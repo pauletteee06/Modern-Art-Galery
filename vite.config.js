0fce97fb1a6a5b413bf95ba424a7a63ac70e85f4
Paulo Rebelo <prebelom@gmail.com>
2025-09-22 14:39:14 +0200

    Fix: Include location.html in build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-    base: '/Modern-Art-Galery/',
+    base: '/Modern-Art-Gallery/',
     build: {
-        outDir: 'dist',
-        assetsDir: 'assets'
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+        input: {
+        main: resolve(__dirname, 'index.html'),
+        location: resolve(__dirname, 'location.html')
+        }
+    }
     }
 })
